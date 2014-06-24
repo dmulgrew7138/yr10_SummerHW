9111130383654e394e0a0199e15d484caa433f37
dmulgrew7138 <dmulgrew7138@sohamcollege.org.uk>
2014-06-24 10:51:27 +0100

    Homework

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,5 @@
 #paste code here
+timesNumber = int(input("What number would you like to times: "))
+numberTO = int(input("How far do you want to go up to: "))
+for x in range(1,(numberTO+1)): 
+    print(timesNumber * x)
